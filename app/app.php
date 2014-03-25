d62d44521afe7389d6e9ead365b351eaba33ecf7
Julien <julien@nitronet.org>
2014-03-26 00:39:38 +0100

    removed app.php -> index.php

--- app/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Fwk\Core\Components\Descriptor\Descriptor;
-
-$desc = new Descriptor(__DIR__ .'/fwk.xml');
-$app = $desc->execute('FwkWWW');
-$app->setDefaultAction('Home');
-
-return $app;
--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 require_once __DIR__ .'/../vendor/autoload.php';
 
-$app = include __DIR__ .'/../app/app.php';
+use Fwk\Core\Components\Descriptor\Descriptor;
+
+$desc = new Descriptor(__DIR__ .'/../app/fwk.xml');
+$app = $desc->execute('FwkWWW');
+$app->setDefaultAction('Home');
 
 $response = $app->run();
 if ($response instanceof \Symfony\Component\HttpFoundation\Response) {
